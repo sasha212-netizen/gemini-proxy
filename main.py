6329b4f85ad7eb84aa753eba0a37e5464224d7f3
sasha212-netizen <sasha20012112@gmail.com>
2026-05-25 01:46:15 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+import os
 from fastapi import FastAPI, Request
 import httpx
 import uvicorn
 
 app = FastAPI()
-GEMINI_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=ТВОЙ_API_КЛЮЧ"
+
+# Берем ключ из настроек окружения на Рендере
+API_KEY = os.environ.get("GEMINI_API_KEY")
+GEMINI_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={API_KEY}"
 
 @app.post("/proxy")
 async def proxy(request: Request):
     data = await request.json()
     async with httpx.AsyncClient() as client:
-        # Пересылаем запрос в Google от имени сервера в США
+        # Пересылаем запрос в Google
         response = await client.post(GEMINI_URL, json=data)
         return response.json()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # Используем порт, который выдает Рендер
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
